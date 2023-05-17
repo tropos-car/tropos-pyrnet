f25d2e7dcfe4c309af5f72179905666116be851d
Jonas Witthuhn <witthuhn@tropos.de>
2023-05-17 16:27:55 +0200

    add l1a->l1b processing

--- src/pyrnet/click.py
+++ src/pyrnet/click.py
@@ -4,8 +4,10 @@ import os.path
 import click
 import numpy as np
 import pandas as pd
+import xarray as xr
 import pkg_resources as pkg_res
 
+from . import pyrnet
 from . import utils
 from . import logger
 from . import reports
@@ -64,7 +66,7 @@ def process_l1a(input_files,
         for fn in files:
             filepath = os.path.abspath(fn)
             filename = os.path.basename(filepath)
-
+            # print(f"{filename}",flush=True)
             m = parse.match(filename)
             try:
                 stationid = int(m.group('ID'))
@@ -81,14 +83,13 @@ def process_l1a(input_files,
                 global_attrs=cfg['global_attrs']
             )
 
-            outfile = os.path.join(output_path, cfg['output'])
+            outfile = os.path.join(output_path, cfg['output_l1a'])
             outfile = outfile.format_map(
                 dict(
                     startdt=pd.to_datetime(ds.time.values[0]),
                     enddt=pd.to_datetime(ds.time.values[-1]),
                     campaign=cfg['campaign'],
                     station=stationid,
-                    level='l1b',
                     collection=cfg['collection'],
                     sfx="nc"
                 )
@@ -97,9 +98,88 @@ def process_l1a(input_files,
 
 
 @click.command("l1b")
-def process_l1b():
-    print("Process l1b")
+@click.argument("input_files", nargs=-1)
+@click.argument("output_path", nargs=1)
+@click.option("--config","-c",
+              multiple=True,
+              help="Specify config files with override the default config.")
+@click.option("--calibration",
+              nargs=1,
+              help="Path to the calibration file. If not specified, pyrnet_calibration.json from pyrnet/share/ is used.")
+@click.option("--mapping",
+              nargs=1,
+              help="Path to the calibration file. If not specified, pyrnet_station_map.json from pyrnet/share/ is used.")
+@click.option("--radflux_varname",
+              nargs=2,
+              default=["dflx_sw", "dflx_sw_2"],
+              help="Dataset variable name of radiation flux. The default is ['dflx_sw','dflx_sw_2'].")
+def process_l1b(input_files: list[str],
+                output_path: str,
+                config:list[str],
+                calibration: str|None,
+                mapping:str|None,
+                radflux_varname:list[str]):
 
+    # read default config
+    fn = pkg_res.resource_filename(__package__, "share/pyrnet_config.json")
+    cfg = utils.read_json(fn)
+
+    # update config with input config files
+    for fn in config:
+        cfg.update(utils.read_json(fn))
+
+    # add default cfmeta if user doesn't specify
+    if not cfg['cfjson']:
+        fn_cfjson = pkg_res.resource_filename("pyrnet", "share/pyrnet_cfmeta_l1b.json")
+        cfg.update({"cfjson": fn_cfjson})
+
+    with click.progressbar(input_files,label='Processing') as files:
+        for fn in files:
+            filepath = os.path.abspath(fn)
+            filename = os.path.basename(filepath)
+            # print(f"{filename}", flush=True)
+            ds = xr.open_dataset(filepath)
+            # check correct file
+            if ds.processing_level != "l1a":
+                raise ValueError(f"{fn} is not a l1a file.")
+
+            box = ds.station.values[0]
+            boxnumber, serial, cfac = pyrnet.meta_lookup(
+                ds.time.values[0],
+                box=box,
+                cfile=calibration,
+                mapfile=mapping,
+            )
+            # print(f"Box:{boxnumber}, Serials: {serial}, Calibration: {cfac}",flush=True)
+
+            # calibrate radiation flux with gain=300
+            for i,radflx in enumerate(radflux_varname):
+                ds[radflx].values = ds[radflx].values*1e6/(cfac[i]* 300) # V -> Wm-2
+                ds[radflx].attrs['units'] = "W m-2",
+                ds[radflx].encoding.update({
+                    'scale_factor': ds[radflx].encoding['scale_factor']*1e6/(cfac[i]* 300)
+                })
+
+
+            ds.attrs["processing_level"] = 'l1b'
+            now = pd.to_datetime(np.datetime64("now"))
+            ds.attrs["history"] = ds.history + f"{now.isoformat()}: Generated level l1a  by pyrnet version XX; " #TODO add version
+
+            udays = np.unique(ds.time.values.astype("datetime64[D]"))
+            for day in udays:
+                day = pd.to_datetime(day)
+                dsd = ds.sel(time=f"{day:%Y-%m-%d}")
+                outfile = os.path.join(output_path, cfg['output_l1b'])
+                outfile = outfile.format_map(
+                    dict(
+                        dt=day,
+                        campaign=cfg['campaign'],
+                        station=box,
+                        collection=cfg['collection'],
+                        sfx="nc"
+                    )
+                )
+                dsd.to_netcdf(outfile)
 
 cli.add_command(process)
 process.add_command(process_l1a)
