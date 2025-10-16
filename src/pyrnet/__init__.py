572932c9c49fddbb472b62ad715fe9bc22362d08
jonas-witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2025-10-16 14:28:08 +0200

    switch to pyproject.toml

--- src/pyrnet/__init__.py
+++ src/pyrnet/__init__.py
@@ -1,3 +1,7 @@
 
-from . import _version
-__version__ = _version.get_versions()['version']
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version("pyrnet")
+except PackageNotFoundError:
+    # package is not installed
+    pass
